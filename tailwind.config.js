ac81b9fa35a00ca8378900a91637eb9dcfc0a2d5
armenarmen <armenlsuny@gmail.com>
2022-10-11 15:33:31 -0600

    rm dependencies in a last ditch insane attempt

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,5 @@ module.exports = {
     },
     container: [],
   },
-  plugins: [require("@tailwindcss/typography"),
-  require('@tailwindcss/forms')
-  ],
+  plugins: [require("@tailwindcss/typography")],
 };
